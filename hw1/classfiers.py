e00590accdbc8d2b1a90e3386c554c648e615a9c
Meng Wu <wum5@umail.iu.edu>
2017-02-20 12:37:27 -0500

    add decision tree

--- hw1/classfiers.py
+++ hw1/classfiers.py
@@ -7,8 +7,8 @@ from sklearn.cross_validation import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn import metrics
+from sklearn import tree
 import matplotlib.pyplot as plt
-from ggplot import *
 
 
 def pre_processing(df):	
@@ -28,29 +28,45 @@ def pre_processing(df):
 	return df
 
 
-def knn_classifier(train, test, cross_val=False):
+def knn_classifier(train,test,k=1,cross_val=False):
 	x_train, y_train = np.array(train.ix[:, :-1]), np.array(train['salary'])
 	x_test, y_test = np.array(test.ix[:, :-1]), np.array(test['salary']) 
 		
-	# here is k-fold validation
+	# here is 10-fold validation
 	if cross_val == True:
 		myList = list(range(1,30))
 		neighbors = filter(lambda x: x%2!=0, myList)
-		cv_scores= []
+		knn_cv_MSE= {}
 		for k in neighbors:
 			knn = KNeighborsClassifier(n_neighbors=k)
 			scores = cross_val_score(knn, x_train, y_train, cv=10, scoring='accuracy')
-			cv_scores.append(scores.mean())
-		MSE = [1-x for x in cv_scores]		
-		plt.plot(neighbors, MSE)
-		plt.xlabel("Number of Neighbors K")
-		plt.ylabel("Misclassfication Error")
-		plt.savefig("KNN.pdf")		
+			knn_cv_MSE[k] = 1-scores.mean()
+		print knn_cv_MSE
 		
 	knn = KNeighborsClassifier(n_neighbors=20)
 	knn.fit(x_train, y_train)
 	preds = knn.predict(x_test)
 	
+	return metrics.accuracy_score(y_test, preds)
+
+
+def decision_tree(train,test,depth=1,cross_val=False):
+	x_train, y_train = np.array(train.ix[:, :-1]), np.array(train['salary'])
+	x_test, y_test = np.array(test.ix[:, :-1]), np.array(test['salary']) 
+	
+	## Perform 7-fold corss validation 
+	if cross_val == True:
+		dt_cv_MSE = {}
+		for i in range(1,20):
+			clf = tree.DecisionTreeClassifier(max_depth=i)
+			scores = cross_val_score(estimator=clf, X=x_train, y=y_train, cv=10)
+			dt_cv_MSE[i] = 1-scores.mean()
+		print dt_cv_MSE
+
+	clf = tree.DecisionTreeClassifier(max_depth=depth)
+	clf = clf.fit(x_train, y_train)
+	
+	preds = clf.predict(x_test)
 	return metrics.accuracy_score(y_test, preds)
 
 
@@ -67,8 +83,10 @@ def main():
 	test_clean = pre_processing(test)
 	print "cleaned training set: %s, raw test set: %s" % (train_clean.shape,test_clean.shape)
 
-	knn_accuracy = knn_classifier(train_clean,test_clean)
-	print "KNN(20) accuracy: %f" % (knn_accuracy)
+	knn_accuracy = knn_classifier(train_clean,test_clean,10)
+	dt_accuracy = decision_tree(train_clean,test_clean,5)
+	print "KNN(k=20) accuracy: %f" % (knn_accuracy)
+	print "Decision tree(d=5) accuracy: %f" % (dt_accuracy)
 
 
 if __name__ == "__main__":
