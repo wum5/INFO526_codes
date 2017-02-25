9bc8a552cd37496b43459c3ae1a05200b166b626
Meng Wu <wum5@umail.iu.edu>
2017-02-25 18:17:12 -0500

    add logistic regression

--- hw1/classfiers.py
+++ hw1/classfiers.py
@@ -15,13 +15,24 @@ import operator
 from sklearn.cross_validation import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
 from sklearn import metrics
 from sklearn import tree
-import matplotlib.pyplot as plt
 from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_selection import RFE
+from sklearn.model_selection import StratifiedKFold
+from sklearn.feature_selection import RFECV
+#import matplotlib.pyplot as plt
+
 
 
 def pre_processing(df):	
+	col_names = ['age','workclass','fnlwgt',\
+	'education','education-num','marital-status','occupation','relationship','race',\
+	'sex','capital-gain','capital-loss','hours-per-week','native-country','salary']
+	df.columns = col_names
+	
 	df = (df.replace(' ?',np.nan)).dropna()
 	df['workclass']=df['workclass'].astype('category')
 	df['education']=df['education'].astype('category')
@@ -39,72 +50,83 @@ def pre_processing(df):
 
 
 def split_x_y(df):
-	x, y = np.array(df.ix[:, :-1]), np.array(df.ix[:, -1])
+	index_x, index_y = df.columns[:-1], df.columns[-1]
+	x = df[index_x]
+	y = df[index_y]
 	return x, y 
 	
+
+def logistic_regression(train,test):
+
+	x_train, y_train = split_x_y(train)
+	x_test, y_test = split_x_y(test)
+
+	# use 10-fold validation to find optimal number of features   
+	model = LogisticRegression()	
+	rfecv = RFECV(estimator=model, step=1, cv=10,
+              scoring='accuracy')   
+	lgr = rfecv.fit(x_train,y_train)
+
+	preds = lgr.predict(x_test)
+	return metrics.accuracy_score(y_test, preds)
+						
 
 def naive_bayes(train,test):
 	x_train, y_train = split_x_y(train)
 	x_test, y_test = split_x_y(test)
 	
-	# need prior feature selection to improve the performance
+	# prior feature selection might improve the performance
 	
-	clf = GaussianNB()
-	clf.fit(x_train, y_train)
+	gnb = GaussianNB()
+	gnb = gnb.fit(x_train, y_train)
 	
-	preds = clf.predict(x_test)
+	preds = gnb.predict(x_test)
 	return metrics.accuracy_score(y_test, preds)
 
 
-def knn_classifier(train,test,k=1,cross_val=False):
+def knn_classifier(train,test):
 	x_train, y_train = split_x_y(train)
 	x_test, y_test = split_x_y(test)
-		
+	
+	best_model = [None,0]  # [model,score] 
+			
 	# here I perform 10-fold cross validation
-	if cross_val == True:
-		myList = list(range(1,30))
-		neighbors = filter(lambda x: x%2!=0, myList)
-		knn_cv_MSE= {}
-		for k in neighbors:
-			knn = KNeighborsClassifier(n_neighbors=k)
-			scores = cross_val_score(knn, x_train, y_train, cv=10, scoring='accuracy')
-			knn_cv_MSE[k] = 1-scores.mean()
-		print knn_cv_MSE
+	neighbors = list(range(1,20))
+	for k in neighbors:
+		knn = KNeighborsClassifier(n_neighbors=k)
+		scores = cross_val_score(knn, x_train, y_train, cv=10, scoring='accuracy')
+		if scores.mean() > best_model[1]:
+			model = knn.fit(x_train,y_train)
+			best_model = [model,scores.mean()]
 		
-	knn = KNeighborsClassifier(n_neighbors=20)
-	knn.fit(x_train, y_train)
+	knn = best_model[0]
 	preds = knn.predict(x_test)
 	
 	return metrics.accuracy_score(y_test, preds)
 
 
-def decision_tree(train,test,depth=1,cross_val=False):
+def decision_tree(train,test):
 	x_train, y_train = split_x_y(train)
 	x_test, y_test = split_x_y(test)
 	
-	## here I perform 10-fold cross validation
-	if cross_val == True:
-		dt_cv_MSE = {}
-		for i in range(1,20):
-			clf = tree.DecisionTreeClassifier(max_depth=i)
-			scores = cross_val_score(estimator=clf, X=x_train, y=y_train, cv=10)
-			dt_cv_MSE[i] = 1-scores.mean()
-		print dt_cv_MSE
-
-	clf = tree.DecisionTreeClassifier(max_depth=depth)
-	clf = clf.fit(x_train, y_train)
+	best_model = [None,0]  # [model,score] 
 	
-	preds = clf.predict(x_test)
+	## here I perform 10-fold cross validation
+	for i in [1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,9,9,9]:   # test decision tree depth
+		dct = tree.DecisionTreeClassifier(max_depth=i)
+		scores = cross_val_score(dct, X=x_train, y=y_train, cv=10)
+		if scores.mean() > best_model[1]:
+			model = dct.fit(x_train,y_train)
+			best_model = [model,scores.mean()]
+	
+	dct = best_model[0]
+	preds = dct.predict(x_test)
 	return metrics.accuracy_score(y_test, preds)
 
 
 def main():
-	train = pd.read_csv('adult.data',header=None,names=['age','workclass','fnlwgt',\
-	'education','education-num','marital-status','occupation','relationship','race',\
-	'sex','capital-gain','capital-loss','hours-per-week','native-country','salary'])
-	test = pd.read_csv('adult.test',header=None,skiprows=[0],names=['age','workclass',\
-	'fnlwgt','education','education-num','marital-status','occupation','relationship',\
-	'race','sex','capital-gain','capital-loss','hours-per-week','native-country','salary'])
+	train = pd.read_csv('adult.data',header=None)
+	test = pd.read_csv('adult.test',header=None,skiprows=[0])
 	print "raw training set: %s, raw test set: %s" % (train.shape,test.shape)
 
 	train_clean = pre_processing(train)
@@ -112,12 +134,17 @@ def main():
 	print "cleaned training set: %s, raw test set: %s" % (train_clean.shape,test_clean.shape)
 
 	nb_accuracy = naive_bayes(train_clean,test_clean)
-	knn_accuracy = knn_classifier(train_clean,test_clean,10)
-	dt_accuracy = decision_tree(train_clean,test_clean,5)
-	
 	print "Naive Bayes accuracy: %f" % (nb_accuracy)
-	print "KNN(k=20) accuracy: %f" % (knn_accuracy)
-	print "Decision tree(d=5) accuracy: %f" % (dt_accuracy)
+
+	knn_accuracy = knn_classifier(train_clean,test_clean)
+	print "KNN accuracy: %f" % (knn_accuracy)
+	
+	dct_accuracy = decision_tree(train_clean,test_clean)
+	print "Decision Tree accuracy: %f" % (dct_accuracy)
+	
+	lgr_accuracy = logistic_regression(train_clean,test_clean)
+	print "Logistic Regression accuracy: %f" % (lgr_accuracy)
+	
 
 
 if __name__ == "__main__":
